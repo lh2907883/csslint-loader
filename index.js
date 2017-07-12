da4fe36c5cb1167117768eb5d3ec0e9f0cdfc2ff
李浩 <hao.li3@renren-inc.com>
2017-07-12 14:30:18 +0800

    添加exclude函数,用来跳过某些文件的检查

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
 var loaderUtils = require("loader-utils");
 var CSSLint = require("csslint").CSSLint;
-
 /**
  * source为原文件的字符串格式
  */
 module.exports = function(source, map) {
-    var query = loaderUtils.getOptions(this) || {
-        force: false,
-        rules: {
-            // 'box-model': 1,
-            'display-property-grouping': 1,
-            'duplicate-properties': 1,
-            'empty-rules': 1,
-            'known-properties': 1,
-            'ids': 1,
-            'multi-rules-newline': 1,
-            'rule-name': 1
+    var query = loaderUtils.getOptions(this);
+    if(!query){
+        query = this._compiler.options.csslint || {
+            force: false,
+            rules: {
+                // 'box-model': 1,
+                'display-property-grouping': 1,
+                'duplicate-properties': 1,
+                'empty-rules': 1,
+                'known-properties': 1,
+                'ids': 1,
+                'multi-rules-newline': 1,
+                'rule-name': 1
+            }
+        };
+    } 
+    /*
+     *  exclude: function(file){
+     *      //file是当前处理的资源路径
+     *      return true; 表示不对当前资源做检查
+     *  }
+     */
+    if(query.exclude){
+        if(query.exclude(this.resourcePath)){
+            return source;
         }
-    };
+    }
     //对source进行解析
     var results = CSSLint.verify(source, query.rules);
     if (results.messages.length > 0) {
         var strError = `In ${this.resourcePath}\n`;
         strError += results.messages.map(item => {
-            return `    ${item.message} line:${item.line}, col:${item.col}, ${item.evidence}\n`;
+            return `    ${item.message} line:${item.line}, @ col:${item.col}, ${item.evidence}\n`;
         }).join('');
         if (query.force === true) {
             this.emitError(strError);
         } else {
-            console.log(strError);
+            this.emitWarning(strError);
         }
     }
     return source;
