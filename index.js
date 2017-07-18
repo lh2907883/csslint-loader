b7e877d102e813a00ade70582ef407cd45d435c2
李浩 <hao.li3@renren-inc.com>
2017-07-18 21:19:22 +0800

    不依赖loaderUtils

--- index.js
+++ index.js
@@ -5,41 +5,38 @@ var CSSLint = require("csslint").CSSLint;
  */
 module.exports = function(source, map) {
     this.cacheable();
-    var query = loaderUtils.getOptions(this);
-    if(!query){
-        query = this._compiler.options.csslint || {
-            force: false,
-            rules: {
-                // 'box-model': 1,
-                'display-property-grouping': 1,
-                'duplicate-properties': 1,
-                'empty-rules': 1,
-                'known-properties': 1,
-                'ids': 1,
-                'multi-rules-newline': 1,
-                'rule-name': 1
-            }
-        };
-    } 
+    var option = this._compiler.options.csslint || {
+        force: false,
+        rules: {
+            // 'box-model': 1,
+            'display-property-grouping': 1,
+            'duplicate-properties': 1,
+            'empty-rules': 1,
+            'known-properties': 1,
+            'ids': 1,
+            'multi-rules-newline': 1,
+            'rule-name': 1
+        }
+    };
     /*
      *  exclude: function(file){
      *      //file是当前处理的资源路径
      *      return true; 表示不对当前资源做检查
      *  }
      */
-    if(query.exclude){
-        if(query.exclude(this.resourcePath)){
+    if(option.exclude){
+        if(option.exclude(this.resourcePath)){
             return source;
         }
     }
     //对source进行解析
-    var results = CSSLint.verify(source, query.rules);
+    var results = CSSLint.verify(source, option.rules);
     if (results.messages.length > 0) {
         var strError = `In ${this.resourcePath}\n`;
         strError += results.messages.map(item => {
             return `    ${item.message} @ line:${item.line}, col:${item.col}, ${item.evidence}\n`;
         }).join('');
-        if (query.force === true) {
+        if (option.force === true) {
             this.emitError(strError);
         } else {
             this.emitWarning(strError);
